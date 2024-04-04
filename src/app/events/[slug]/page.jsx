644c774cfcca28dd82301e299b9e7a811e7791aa
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-04-04 07:36:41 +0530

    feat:dynamic import

--- src/app/events/[slug]/page.jsx
+++ src/app/events/[slug]/page.jsx
@@ -4,16 +4,24 @@ import Credits from '@/widgets/Credits'
 import EventListPage from '@/widgets/EventListPage'
 import Footer from '@/widgets/Footer'
 // import { useRouter } from 'next/router'
-import React from 'react'
+import React, { Suspense } from 'react'
+import dynamic from 'next/dynamic'
+
+const DynamicHeader = dynamic(() => import('@/widgets/EventListPage'), {
+  loading: () => <p>Loading...</p>,
+})
 
 export default function page() {
   // const router = useRouter();
   // console.log(router.query)
   return (
     <>
-    <EventListPage/>
-    <Footer/>
-    <Credits/>
+      <Suspense fallback={<div>loading</div>}>
+        <DynamicHeader />
+        {/* <EventListPage /> */}
+      </Suspense>
+      <Footer />
+      <Credits />
     </>
   )
 }
--- src/common/components/EventList.jsx
+++ src/common/components/EventList.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import styles from '@styles/scss/eventList.module.scss'
 import Image from 'next/image'
+
 export default function EventList({ eventList }) {
   return (
     <div className={styles.container}>
       {eventList?.map((event) => (
-        <Image key={event?._id} src={event?.imgUrl} height={1000} width={1000} className={styles.poster}/>
+        <Suspense fallback={"loading"}>
+          <Image key={event?._id} src={event?.imgUrl} height={1000} width={1000} className={styles.poster} />
+        </Suspense>
       ))}
     </div>
   )
