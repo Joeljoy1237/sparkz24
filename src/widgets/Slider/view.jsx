626b58fb9a339cb2d976c7a9574aa02b9b3a599a
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-04-10 22:58:06 +0530

    feat:tap highlight remove

--- src/widgets/Slider/view.jsx
+++ src/widgets/Slider/view.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '@styles/scss/slider.module.scss'
 import LeftIcon from '@/common/icons/Left'
 import RightIcon from '@/common/icons/RightIcon'
+import { toast } from 'react-toastify'
 
 export default function Slider({
     events
@@ -12,7 +13,12 @@ export default function Slider({
         <div className={styles.container}>
             <div className={styles.sliderContainer}>
                 {events?.map((event) => (
-                    <Image className={styles.poster} src={event?.imgUrl ? event?.imgUrl : event?.posterImg} height={1000} width={1000} />
+                    <Image onClick={()=>{
+                        toast.info("Registration starts soon.",{
+                          position:"bottom-center",
+                          theme:"dark"
+                        })
+                      }} className={styles.poster} src={event?.imgUrl ? event?.imgUrl : event?.posterImg} height={1000} width={1000} />
                 ))}
             </div>
         </div>
