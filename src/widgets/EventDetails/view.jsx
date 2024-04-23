c4660b6131f995099837288a509847b85a663ca5
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-04-23 06:34:13 +0530

    f

--- src/widgets/EventDetails/view.jsx
+++ src/widgets/EventDetails/view.jsx
@@ -79,7 +79,7 @@ export default function EventDetails() {
                                                             }
                                                             {event?.priceCount >= 3 &&
                                                                 <div className={styles.prize}>
-                                                                    <Image width={1000} height={1000} className={styles.prizeicon} src={"/images/third.png"} />
+                                                                    <Image width={1000} height={1000} className={styles.prizeicon3} src={"/images/third.png"} />
                                                                     <span className={styles.prizeAmt}>{event?.thirdPrize}</span>                                        </div>
                                                             }
                                                         </div>
